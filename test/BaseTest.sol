b3e53528dcb49b1777126601164e07523f48ea40
James Wenzel <jameswenzel@users.noreply.github.com>
2022-12-21 13:32:27 -0800

    add base test and license to factory interface

---
+++ test/BaseTest.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import {Test} from "forge-std/Test.sol";
+
+contract BaseTest is Test {
+    function setUp() public virtual {}
+
+    function testThing() public {}
+}
